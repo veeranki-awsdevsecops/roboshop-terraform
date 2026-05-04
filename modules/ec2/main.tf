eb9cec550d7357a069038e311b99962c4afce577
windows <veeranki2014@gmail.com>
2026-05-03 22:38:27 -0500

    roboshop project with tools

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -41,7 +41,7 @@ resource "null_resource" "ansible" {
       host     = aws_instance.instance.private_ip
     }
     inline = [
-      "sudo pip3.12 install ansible",
+      "sudo pip3.11 install ansible",
       "ansible-pull -i localhost, -U https://github.com/veeranki-awsdevsecops/roboshop-ansible roboshop.yml -e role_name=${var.name}"
     ]
   }
