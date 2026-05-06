c5b9f2d44e9c24698aef7cb5decd58f0b71d4655
windows <veeranki2014@gmail.com>
2026-05-05 19:11:48 -0500

    roboshop project with tools

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -42,7 +42,7 @@ resource "null_resource" "ansible" {
     }
     inline = [
       "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/veeranki-awsdevsecops/roboshop-ansible roboshop.yml -e role_name=${var.name} -e token=${var.token}"
+      "ansible-pull -i localhost, -U https://github.com/veeranki-awsdevsecops/roboshop-ansible roboshop.yml -e role_name=${var.name} -e token=${var.token} -e env=${var.env}"
     ]
   }
 }
