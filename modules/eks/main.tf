845eafcffd3fe5e73e43237244a252f1f798d166
windows <veeranki2014@gmail.com>
2026-08-11 18:15:10 -0400

    roboshop project with tools

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -7,32 +7,6 @@ resource "aws_eks_cluster" "main" {
   }
 }
 
-resource "aws_iam_role" "node" {
-  name = "eks-auto-node-example"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = ["sts:AssumeRole"]
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodeMinimalPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy"
-  role       = aws_iam_role.node.name
-}
-
-resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryPullOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
-  role       = aws_iam_role.node.name
-}
-
 resource "aws_iam_role" "cluster" {
   name = "eks-cluster-example"
   assume_role_policy = jsonencode({
