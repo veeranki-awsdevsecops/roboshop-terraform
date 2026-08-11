eb6a0031e904237aeab8af0a8b20b46e6db3ea2e
windows <veeranki2014@gmail.com>
2026-08-11 16:52:24 -0400

    roboshop project with tools

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
-module "ec2" {
-  for_each   = var.instances
-  source     = "./modules/ec2"
-  ami        = var.ami
-  env        = var.env
-  instance_type = each.value["instance_type"]
-  name = each.key
-  zone_id = var.zone_id
-  token = var.token
+# module "ec2" {
+#   for_each   = var.instances
+#   source     = "./modules/ec2"
+#   ami        = var.ami
+#   env        = var.env
+#   instance_type = each.value["instance_type"]
+#   name = each.key
+#   zone_id = var.zone_id
+#   token = var.token
+# }
+
+module "eks" {
+  source = "../modules/eks"
 }
--- tools/main.tf
+++ tools/main.tf
@@ -1,15 +1,12 @@
-# module "tools" {
-#   for_each = var.tools
-#   source = "../modules/ec2"
-#   ami    = var.ami
-#   env    = null
-#   instance_type = each.value["instance_type"]
-#   name = each.key
-#   zone_id = var.zone_id
-#   token = var.token
-# }
-
-module "eks" {
-  source = "../modules/eks"
+module "tools" {
+  for_each = var.tools
+  source = "../modules/ec2"
+  ami    = var.ami
+  env    = null
+  instance_type = each.value["instance_type"]
+  name = each.key
+  zone_id = var.zone_id
+  token = var.token
 }
+
 
